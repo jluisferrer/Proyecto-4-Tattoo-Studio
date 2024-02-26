262cf9fed7bee2880c331b4d5df52fce18e89a43
jluisferrer <jluisferrer@gmail.com>
2024-02-26 20:43:20 +0100

    feat:Create tables.ts on migrations

--- src/database/migrations/1708971478080-appointments.ts
+++ src/database/migrations/1708971478080-appointments.ts
@@ -1,11 +1,56 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class Appointments1708971478080 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "appointments",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "appointment_date",
+                        type: "date",
+                        isNullable: false,
+                    },
+                    {
+                        name: "user_id",
+                        type: "int",
+                        isNullable: false,
+                    },
+                    {
+                        name: "service_id",
+                        type: "int",
+                        isNullable: false,
+                    },
+                ],
+                foreignKeys: [
+                    {
+                        columnNames: ["user_id"],
+                        referencedTableName: "users",
+                        referencedColumnNames: ["id"],
+                        onDelete: "CASCADE"
+                    },
+                    {
+                        columnNames: ["service_id"],
+                        referencedTableName: "services",
+                        referencedColumnNames: ["id"],
+                        onDelete: "CASCADE"
+                    }
+                ]
+            }),
+            true          //IF NOT EXIST
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("appointments");
     }
 
 }
--- src/database/migrations/1708971492544-services.ts
+++ src/database/migrations/1708971492544-services.ts
@@ -1,11 +1,37 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class Services1708971492544 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "services",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "service_name",
+                        type: "varchar",
+                        length: "255",
+                    },
+                    {
+                        name: "description",
+                        type: "text",
+                        isNullable: true,
+                    },
+                ],
+            }),
+            true          //IF NOT EXIST
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("services");
     }
 
 }
