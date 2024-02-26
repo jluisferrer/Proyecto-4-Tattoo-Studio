c9b14ad15b5d396040fcf3637e862b6e51969c11
jluisferrer <jluisferrer@gmail.com>
2024-02-26 19:09:19 +0100

    feat:database initialization

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import express from "express";
+
+
+
+export const app = express(); 
+
+app.use(express.json());
---
+++ src/database/db.ts
@@ -0,0 +1,15 @@
+import "reflect-metadata"
+import "dotenv/config";
+import { DataSource } from "typeorm"
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "",
+    database: "test",
+    entities: [],
+    synchronize: false,
+    logging: false,
+    })
