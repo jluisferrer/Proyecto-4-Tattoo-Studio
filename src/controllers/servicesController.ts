78d02be750fbf3b621a219a8f11dbe4b0d77ce1a
jluisferrer <jluisferrer@gmail.com>
2024-03-27 23:13:23 +0100

    update:naming of success updateUser

--- src/controllers/servicesController.ts
+++ src/controllers/servicesController.ts
@@ -5,14 +5,14 @@ export const getServices = async (req: Request, res: Response) => {
     try {
         const services = await Service.find()
         res.status(200).json({
-            succes: true,
+            success: true,
             message: "Services retrieved succesfully",
             data: services
         })
     }
     catch (error) {
         res.status(500).json({
-            succes: false,
+            success: false,
             message: "Service cant be retrieved",
             error: error
         })
